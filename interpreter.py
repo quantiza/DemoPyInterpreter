0fedc58fb555d8d406e97c18fd85cafab678801e
LongYang <LongYang@192.168.0.8>
2017-11-16 16:34:28 +0800

    使用动态解释命令

--- interpreter.py
+++ interpreter.py
@@ -1,3 +1,23 @@
+# -*- coding: utf-8 -*-  
+# --------------------------------
+# hasattr(object, name)
+# 判断一个对象里面是否有name属性或者name方法，返回BOOL值，有name特性返回True， 否则返回False。
+# 需要注意的是name要用括号括起来
+# 如 hasattr(datetime.datetime, "now") -> True
+# -------------------------------- 
+# getattr(object, name[,default])
+# 获取对象object的属性或者方法，如果存在打印出来，如果不存在，打印出默认值，默认值可选。
+# 需要注意的是，如果是返回的对象的方法，返回的是方法的内存地址，如果需要运行这个方法，
+# 可以在后面添加一对括号。
+# 如 getattr(datetime.datetime, "now") -> now()
+# ---------------------------------
+# setattr(object, name, values)
+# 给对象的属性赋值，若属性不存在，先创建再赋值。
+# ----------------------------------
+
+
+import datetime
+
 what_to_execute = {
     "instructions": [("LOAD_VALUE", 0),
                      ("STORE_NAME", 0),
@@ -55,16 +75,15 @@ class Interpreter(object):
         for each_step in instructions:
             instruction, argument = each_step
             argument = self.parse_argument(instruction, argument, what_to_execute)
-            if instruction == "LOAD_VALUE":
-                self.LOAD_VALUE(argument)
-            elif instruction == "ADD_TWO_VALUES":
-                self.ADD_TWO_VALUES()
-            elif instruction == "PRINT_ANSWER":
-                self.PRINT_ANSWER()
-            elif instruction == "STORE_NAME":
-            	self.STORE_NAME(argument)
-            elif instruction == "LOAD_NAME":
-            	self.LOAD_NAME(argument)
+            bytecode_method = getattr(self, instruction)
+            if argument is None:
+            	bytecode_method()
+            else:
+            	bytecode_method(argument)
+
+
 
 interpt = Interpreter()
 interpt.run_code(what_to_execute)
+
+
